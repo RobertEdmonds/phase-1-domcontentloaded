ca93a55a137810a362d9af608594c87f981d2d60
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-11 17:14:19 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    let change = document.querySelector('#text')
+    change.textContent = "This is really cool!"
+})
